82bb0ba929ae5cfb5b87733e303c0b794cf1451a
Muhammad Izzuddin Al Fikri <saiko.mizuki@gmail.com>
2025-06-14 12:44:06 +0700

    setup

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), vue()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
