481cf7b9867adce7051d08dffce1c268414b37ce
Muhammad Izzuddin Al Fikri <saiko.mizuki@gmail.com>
2025-06-15 20:19:26 +0700

    wip: homepage content; footer styling

---
+++ src/components/vue/ExampleBanner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ message }}</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    message: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<style scoped>
+div {
+    padding: 8px;
+    border: 1px solid #aaa;
+    border-radius: 4px;
+    background: #eee;
+}
+</style>
